eb8131072b29a3353db2bc6e96be76877d7505d8
Alejandro Contreras <alejandro.contreras@REM-MC-210426J.local>
2021-04-30 14:06:36 -0500

    env added

--- database.py
+++ database.py
@@ -1,9 +1,11 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://alexcd:Al17Con09@localhost/polyglot"
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
