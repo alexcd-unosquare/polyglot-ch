7ebb057cd0af5b6d799faeea0693cfad636f7911
Alejandro Contreras <alejandro.contreras@REM-MC-210426J.local>
2021-04-30 14:12:59 -0500

    db env fixed

--- database.py
+++ database.py
@@ -5,7 +5,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
+uri = os.getenv('DATABASE_URL')
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+SQLALCHEMY_DATABASE_URL = uri
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
